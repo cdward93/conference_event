9d5b12be777879ff4878bc9d8671ed5fec0f1577
johnwendel <john.alan.wendel@gmail.com>
2024-10-23 21:48:14 -0400

    av addons added

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -25,21 +25,6 @@ const ConferenceEvent = () => {
 
 
 
-    {avItems.map((item, index) => (
-    <div className="av_data venue_main" key={index}>
-        <div className="img">
-            <img src={item.img} alt={item.name} />
-        </div>
-    <div className="text"> {item.name} </div>
-    <div> ${item.cost} </div>
-        <div className="addons_btn">
-            <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
-            <span className="quantity-value">{item.quantity}</span>
-            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
-        </div>
-    </div>
-))}
-
 
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
@@ -208,6 +193,27 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
+
+
+
+    {avItems.map((item, index) => (
+    <div className="av_data venue_main" key={index}>
+        <div className="img">
+            <img src={item.img} alt={item.name} />
+        </div>
+    <div className="text"> {item.name} </div>
+    <div> ${item.cost} </div>
+        <div className="addons_btn">
+            <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+            <span className="quantity-value">{item.quantity}</span>
+            <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+        </div>
+    </div>
+))}
+
+
+
+
 
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
